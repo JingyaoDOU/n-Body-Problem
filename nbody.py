947b3634fe595f0ae08e0ea3b83e7129c77b86ae
JingyaoDOU <53231050+JingyaoDOU@users.noreply.github.com>
2019-08-19 23:21:26 +0100

    Add files via upload

--- nbody.py
+++ nbody.py
@@ -1,14 +1,14 @@
 # Import libraries
-from tqdm import tqdm
 import numpy as np 
 from numpy import linalg as LA
 from math import sqrt,pi
 from scipy.optimize import minimize
 
-# Define the gravitational potential energy function between two body with mass m1 and m2
+
 global G
 G = 1.0
 
+# Define the gravitational potential energy function between two body with mass m1 and m2
 def UG(r,m1,m2): 
     
     # UG(r) = -GMm/r
@@ -17,8 +17,10 @@ def UG(r,m1,m2):
     
     return potential
 
-
-def derivatives_U(q1,q2,m1,m2):
+# The force and energy between two bodies with mass m1 and m2
+# F is a (1,2) array, with F[0] represents force in x direction
+# F[1] represents force in y direction
+def gravi_force(q1,q2,m1,m2):
     
     r = np.linalg.norm( q1-q2 )
     
@@ -39,22 +41,14 @@ def derivatives_U(q1,q2,m1,m2):
     dnorm_dq2x = -(q1x-q2x)/r 
     dnorm_dq2y = -(q1y-q2y)/r
     
-
     du_dq1x = dU_dr * dnorm_dq1x
     du_dq1y = dU_dr * dnorm_dq1y
     
     du_dq2x = dU_dr * dnorm_dq2x
     du_dq2y = dU_dr * dnorm_dq2y
     
-    return energy, du_dq1x, du_dq1y, du_dq2x, du_dq2y
-
-# the force between to bodies with mass m1 and m2
-def gravi_force(q1,q2,m1,m2):
-    
     F1 = np.zeros_like(q1) 
     F2 = np.zeros_like(q2)
-    
-    energy, du_dq1x, du_dq1y, du_dq2x, du_dq2y = derivatives_U(q1,q2,m1,m2)
     
     F1[0] = -du_dq1x
     F1[1] = -du_dq1y
@@ -63,11 +57,12 @@ def gravi_force(q1,q2,m1,m2):
     
     return energy, F1, F2 
 
-# let q be the vector containing position information of all the particles. 
-# Each row repesent a particle and columns represent corresponding to x or y cooridnates 
-# the last column represents the mass of that particle 
+# Given position and velocity of n bodies in the system
+# Output the force on each body, potential energy and angular momentum of the system
+# The structure of q is (x1,y1,x2,y2,...,xn,yn)
+# q and f ara of the similar form (f1x,f1y,f2x,f2y,...,fnx,fny)
 
-def total_force(q,p,M,N):
+def G_force(q,p,M,N):
     pe=0
     L=0
     F=np.zeros((N,N,2))
@@ -96,7 +91,65 @@ def total_force(q,p,M,N):
     f=np.squeeze(f)
     #f = np.hstack(f) 
     return pe, f ,L
+
+
+# Update the position and velocity with different integrator.
+
+# Output new postion, velocity, total energy and angular momentum
+# of the new system after time step h.
+
+def Eulers_Method(q,p,M,N,h, force_function):
     
+    # Compute the force
+    pe_old, f,L_old = force_function(q,p,M,N)
+
+    # Do the update
+    qt = q + h * p 
+    pt = p + h * f
+    
+    # Compute the new energies
+    pe, f_new, L = force_function(qt,pt,M,N)
+    ke = np.sum( pt*pt / 2*np.repeat(M,2)) 
+    
+    # Total energy is kinetic + potential
+    total_e = ke + pe 
+    
+    # Return values 
+    return qt, pt, total_e,L
+
+def Verlet(q,p,M,N,h, force_function):
+    
+    old_pe,f,old_L=force_function(q,p,M,N)
+    
+    pt=p+(h/2)*f
+    qt=q+h*pt
+    new_pe,f,new_L=force_function(qt,pt,M,N)
+    pt=pt+(h/2)*f
+    
+    pe, f_new,L = force_function(qt,pt,M,N)
+    ke = np.sum( pt*pt / 2*np.repeat(M,2)) 
+    
+    # Total energy is kinetic + potential
+    total_e = ke + pe 
+    
+    # Return values 
+    return qt, pt, total_e, L
+
+def Leapfrog(q,p,M,N,h,force_function):
+    
+    qt=q+h*p
+    old_pe,f,old_L=force_function(qt,p,M,N)
+    pt=p+h*f
+    
+    #q_half=(q+qt)/2
+    p_half=(p+pt)/2
+    
+    pe,f,L=force_function(qt,p_half,M,N)
+    ke = np.sum( p_half*p_half / 2*np.repeat(M,2)) 
+    
+    total_e = ke + pe 
+    return qt, pt, total_e,L
+
 def Verlet(q,p,M,N,h, force_function):
     
     old_pe,f,old_L=force_function(q,p,M,N)
@@ -115,18 +168,122 @@ def Verlet(q,p,M,N,h, force_function):
     # Return values 
     return qt, pt, total_e, L
     
-def run_check(q0, p0,M,N, Nsteps,tlim, h, step_function, force_function):
+def RK4(q,p,M,N,h, force_function):
+    
+    p1=np.copy(p)
+    q1=np.copy(q)
+    pe1,f1,L1=force_function(q1,p1,M,N)
+    
+    p2=p+(h/2)*f1
+    q2=q+(h/2)*p1
+    pe2,f2,L2=force_function(q2,p2,M,N)
+    
+    p3=p+(h/2)*f2
+    q3=q+(h/2)*p2
+    pe3,f3,L3=force_function(q3,p3,M,N)    
+    
+    p4=p+h*f3
+    q4=q+h*p3
+    pe4,f4,L4=force_function(q4,p4,M,N)
+    
+    qt=q+(h/6)*(p1+2*p2+2*p3+p4)
+    pt=p+(h/6)*(f1+2*f2+2*f3+f4)
+    
+    pe, f_new,L = force_function(qt,pt,M,N)
+    ke = np.sum( pt*pt / 2*np.repeat(M,2)) 
+    
+    total_e = ke + pe 
+    
+    return qt, pt, total_e, L, f_new
 
-    # Set initial conditions
+def motion_simulation(q0, p0, M, Nsteps, tlim=1e10, h, step_function, force_function, adaptive=False, lamda=50):
+    
+
+    if (step_function=='Leapfrog'):
+        h_ini=h/20
+
+        N_ini=10
+        for n in range(N_ini):
+        Q,p0,energy,L = Eulers_Method(q0, p0,M,N, h_ini, total_force)
+
+    N=len(M)
     q = np.copy(q0)
     p = np.copy(p0)
+
+    # Find initial totoal energy and angular momentum
+    pe_ini,f_ini,L_ini=total_force(q,p,M,N)
+    ke_ini = np.sum( p*p / 2*np.repeat(M,2)) 
+    e_ini=pe_ini+ke_ini
+    
+    # shift coordinates to the system where center of mass is fixed at the origin
+    q_center=np.array([np.sum(M*q[::2]),np.sum(M*q[1::2])])/np.sum(M)
+    q_tran_ini=q-np.tile(q_center,N)
+    p_center=np.array([np.sum(p[::2]),np.sum(p[1::2])])/np.sum(M)        
+    p_tran_ini=p-np.tile(p_center,N)
            
     t = 0
 
-    q_traj = [] 
-    D_traj = []
+    q_traj = [q_tran] 
+    p_traj = [p_tran] 
+    e_traj = [e_ini]
+    L_traj = [L_ini]
+    t_traj = [0]
+    
+    # Main loop
+    for n in range(Nsteps):
+        
+        t = t + h 
+        if t>tlim:
+            break
+    
+        q,p,energy,L,f = step_function(q, p,M,N, h, force_function)
+        q_center=np.array([np.sum(M*q[::2]),np.sum(M*q[1::2])])/np.sum(M)
+        q_tran=q-np.tile(q_center,N)
+        p_center=np.array([np.sum(p[::2]),np.sum(p[1::2])])/np.sum(M)        
+        p_tran=p-np.tile(p_center,N)
 
-    h_copy=np.copy(h)
+        if (adaptive):
+            r=[]
+            v=[]
+            for i in range(N-1):
+                q1=q[2*i:2*i+2]
+                p1=q[2*i:2*i+2]
+                for j in range (i+1,N):
+                    q2=q[2*j:2*j+2]
+                    p2=q[2*j:2*j+2]
+                    r+=np.linalg.norm(q1-q2)
+                    v+=np.linalg.norm(p1-p2)
+            r=np.array(r)
+            v=np.array(v)
+            dh=r/v
+            h=np.min(dh)/lamda
+
+        # Save the system's data
+        q_traj += [q_tran] 
+        p_traj += [p_tran] 
+        e_traj += [energy] 
+        L_traj += [L] 
+        t_traj += [t] 
+
+    # Format into numpy arrays
+    q_traj = np.array(q_traj)
+    p_traj = np.array(p_traj) 
+    L_traj = np.array(L_traj) 
+    e_traj = np.array(e_traj) 
+
+    # Return the trajectories
+    return  q_traj, p_traj, e_traj, t_traj,L_traj
+
+def stability_check(dx, dy, M, Nsteps,tlim, h, step_function, force_function, adaptive=True, lamda=50):
+
+    q,p=findVV(dx,dy)
+
+    N=len(M)
+
+    t = 0
+
+    q_traj = []
+    D_traj = []
     
     # Main loop
     for n in range(Nsteps):
@@ -136,36 +293,32 @@ def run_check(q0, p0,M,N, Nsteps,tlim, h, step_function, force_function):
             break
         # Step in time
         q,p,energy,L = step_function(q, p,M,N, h, force_function)
-
-        q_tran=q
-        p_tran=p
         
-        r1=np.linalg.norm(q_tran[0:2]-q_tran[2:4])
-        r2=np.linalg.norm(q_tran[0:2]-q_tran[4:6])
-        r3=np.linalg.norm(q_tran[2:4]-q_tran[4:6])
+        r1=np.linalg.norm(q[0:2]-q[2:4])
+        r2=np.linalg.norm(q[0:2]-q[4:6])
+        r3=np.linalg.norm(q[2:4]-q[4:6])
         r_tol=r1+r2+r3
         
         D12=r1/r_tol
         D13=r2/r_tol
         D=np.array([D12,D13])
         
-        dv1=np.linalg.norm(p_tran[0:2]-p_tran[2:4])
-        dv2=np.linalg.norm(p_tran[0:2]-p_tran[4:6])
-        dv3=np.linalg.norm(p_tran[2:4]-p_tran[4:6])
+        dv1=np.linalg.norm(p[0:2]-p[2:4])
+        dv2=np.linalg.norm(p[0:2]-p[4:6])
+        dv3=np.linalg.norm(p[2:4]-p[4:6])
         
         h1=r1/dv1
         h2=r2/dv2
         h3=r3/dv3
-    
-        h=np.min(np.array([h1,h2,h3]))/50
+
+        if (adaptive):
+            h=np.min(np.array([h1,h2,h3]))/lamda
                 
         rmax=np.max(np.array([r1,r2,r3]))
-        
-        D_traj += [D]
-        
         if rmax>=10.0:
             break
-      
+        D_traj += [D]
+
     D_traj = np.array(D_traj)
     
     countbox=countBox(D_traj)
@@ -180,8 +333,14 @@ def countBox(q_traj):
     Nbox=np.sum(A)
     return Nbox
 
-def findVV(q0,p0,dx,dy,e_ini):
+def findVV(dx,dy):
     
+    q0=np.array([-0.97000436, 0.24308753,0.0,0.0, 0.97000436, -0.24308753])
+    p0=np.array([0.4662036850, 0.4323657300, -0.93240737, -0.86473146, 0.4662036850, 0.4323657300])
+    pe_ini,f_ini,L_ini=total_force(q0,p0,M,N)
+    ke_ini = np.sum( p0*p0 / 2*np.repeat(M,2)) 
+    e_ini=pe_ini+ke_ini
+
     dxy=np.hstack([dx,dy])
     q1_ini=-q0[0:2]
     q2_ini=-q1_ini
@@ -204,38 +363,62 @@ def findVV(q0,p0,dx,dy,e_ini):
     
     return q_final,p_final
 
-
-
-
-p0=np.array([0.4662036850, 0.4323657300, -0.93240737, -0.86473146, 0.4662036850, 0.4323657300])
-q0 =np.array([-0.97000436, 0.24308753,0.0,0.0, 0.97000436, -0.24308753])
-M=np.array([1.0,1.0,1.0])
-N=3
-
-pe_ini,f_ini,L_ini=total_force(q0,p0,M,N)
-ke_ini = np.sum( p0*p0 / 2*np.repeat(M,2)) 
-e_ini=pe_ini+ke_ini
-
-h = 0.002
-Nsteps = 500000
-tlim=2000
-
-dx2=np.linspace(-0.2,0.2,101)
-dy2=np.linspace(-0.2,0.2,101)
-i=0
-j=0
-island1=np.zeros((101,101))
-island2=np.zeros((101,101))
-
+def Action_function(x,N,n,m):
+    lagTime=m/N
+    dt=2*pi/m
+    x_traj=np.zeros((N,m))
+    y_traj=np.zeros((N,m))
+    vx_traj=np.zeros((N,m))
+    vy_traj=np.zeros((N,m))
+    for i in ( range(N)):
+        for t in ( range(m)):
+            x_traj[i,t]=x[0]+np.sum(x[1:2*n+1]*sincos_1(n,((t+i*lagTime)%m)*dt))
+            y_traj[i,t]=x[2*n+1]+np.sum(x[2*n+2:]*sincos_1(n,((t+i*lagTime)%m)*dt))
+            
+    for i in ( range(N)):
+        for t in ( range(m)):
+            vx_traj[i,t]=x[0]+np.sum(x[1:2*n+1]*sincos_2(n,((t+i*lagTime)%m)*dt))
+            vy_traj[i,t]=x[2*n+1]+np.sum(x[2*n+2:]*sincos_2(n,((t+i*lagTime)%m)*dt))
+            
+    K=0.5*np.sum(vx_traj**2+vy_traj**2,axis=0)
+    U=0
+    for i in range(N-1):
+        for j in range(i+1,N):
+            r=np.sqrt((x_traj[i,:]-x_traj[j,:])**2+(y_traj[i,:]-y_traj[j,:])**2)
+            U=U-1/r
+            
+    A=np.sum(K-U)*dt
     
-for dx in tqdm( dx2 ):
-    for dy in dy2:
-        q_final,p_final=findVV(q0,p0,dx,dy,e_ini)
-        island1[i,j],island2[i,j]=run_check(q_final, p_final,M,N, Nsteps,tlim, h, Verlet, total_force)
-        j+=1
-    i+=1
-    j=0
-    
+    return A
 
-np.save('island1.npy',island1)
-np.save('island2.npy',island2)
+def sincos_1(n,t):
+    return np.hstack([np.sin(np.arange(1,n+1)*t),np.cos(np.arange(1,n+1)*t)])
+
+def sincos_2(n,t):
+    return np.hstack([np.arange(1,n+1)*np.cos(np.arange(1,n+1)*t),
+                      -np.arange(1,n+1)*np.sin(np.arange(1,n+1)*t)])
+
+def FFT8(n):
+    q0=np.array([-0.07880227334416882, 0.5570371897354746,0.5940359608209828, 0.383319210563721,-0.5152336874768139, -0.9403564002991956])
+    p0=np.array([0.15998292728488323, 1.1593418791674066,-0.5557289806160467, -0.9029539156799118,0.39574605333116347, -0.2563879634874948])
+    M=np.array([1.0,1.0,1.0])
+
+    h = 2*pi/n
+
+    T = 2*pi
+    tlim=1000
+    Nsteps = n
+    N=3
+    
+    f_traj,q_traj, p_traj, e_traj, t_traj ,L_traj= run_simulation(q0, p0,M,N, Nsteps,tlim, h, RK4, total_force)
+    
+    a2=np.real(np.fft.rfft(q_traj[:,0]))
+    a1=np.imag(np.fft.rfft(q_traj[:,0]))
+    
+    b2=np.real(np.fft.rfft(q_traj[:,1]))
+    b1=np.imag(np.fft.rfft(q_traj[:,1]))
+    
+    x0=np.hstack([a2[0],a1[1:],a2[1:],b2[0],b1[1:],b2[1:]])
+    return x0
+
+
